ba04815cc6c6ef23fc5d597c9a42cfafe53550e4
keshan <keshan011@live.com>
2021-03-01 08:08:17 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 import requests
 from graphmk.version import Version
 from os.path import basename, exists, dirname, abspath, join
+from setuptools import find_packages
 
 
 graphmk_version = '1.0.0'
@@ -16,22 +17,33 @@ def download_jars(datapath, version=graphmk_version):
 
 download_jars(datapath=DATAPATH)
 
-setuptools.setup(name='py-graphmk',
-                 version=Version('1.0.0').number,
-                 description='Python Wrapper for GraphMk',
-                 long_description=open('README.md').read().strip(),
-                 author='keshan',
-                 author_email='keshann.18@cse.mrt.ac.lk',
-                 url='https://github.com/graphmk/py-graphmk',
-                 py_modules=['graphmk'],
-                 install_requires=[
+setuptools.setup(
+                name='graphmk',
+                version=Version('1.0.0').number,
+                description='Python Wrapper for GraphMk',
+                long_description=open('README.md').read().strip(),
+                author='keshan',
+                author_email='keshann.18@cse.mrt.ac.lk',
+                url='https://github.com/graphmk/py-graphmk',
+                py_modules=['graphmk/graphmk', 'graphmk/__init__'],
+                data_files=[('graphmk/lib', ['graphmk/lib/graphmk-{version}.jar'.format(version=graphmk_version)])],
+                install_requires=[
                     'pytest',
                     'pytest-cov',
                     'JPype1',
                     'chardet',
                     'request'
-                 ],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='boilerplate package',
-                 classifiers=['Packages', 'Boilerplate'])
+                ],
+                license='MIT License',
+                zip_safe=False,
+                keywords=('graphmk','graph','extraction','analytics','relational','database'),
+                classifiers=[       
+                    'Development Status :: 5 - Production/Stable',
+                    'Environment :: Console',
+                    'Intended Audience :: Developers',
+                    'License :: OSI Approved :: Apache Software License',
+                    'Operating System :: OS Independent',
+                    'Programming Language :: Python :: 2.7',
+                    'Natural Language :: English'
+                    ]
+            )
